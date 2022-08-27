e4171c2756a52d14deeae6b90a8e4bfb6270271d
axelguerrero <guerreroaxel230@yahoo.com>
2022-08-27 14:34:13 -0400

    made a readme file and also wrote a function to add images to my game

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //Learned how to set up a canvas from Stackoverflow
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+let canvas = document.querySelector('canvas')
+let c = canvas.getContext('2d')
 
 canvas.width = 1280
 canvas.height = 720
 //I used this stack over flow link for color "/questions/27736288/how-to-fill-the-whole-canvas-with-specific-color"
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+//Background
