08124cc5c3db089d9c0cf7bcb9f95ee6b4f6f1ae
axelguerrero <guerreroaxel230@yahoo.com>
2022-08-29 22:42:50 -0400

    changed nav font and and added a new func

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-//Learned how to set up a canvas from Stackoverflow
+//Learned how to set up a canvas from https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
 let canvas = document.querySelector('canvas')
 let c = canvas.getContext('2d')
 
 canvas.width = 1280
 canvas.height = 720
-//I used this stack over flow link for color "/questions/27736288/how-to-fill-the-whole-canvas-with-specific-color"
+
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-//Background
+//Function to add images
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
