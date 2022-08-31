42c8a2d145a9a331086c8954aa52398610afade8
axelguerrero <guerreroaxel230@yahoo.com>
2022-08-31 18:54:58 -0400

    corrected punctuation

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 //Learned how to set up a canvas from https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
-let canvas = document.querySelector('canvas')
-let c = canvas.getContext('2d')
-
+const canvas = document.querySelector("canvas")
+const c = canvas.getContext("2d")
+//dimensions
 canvas.width = 1280
 canvas.height = 720
 
+//fills in canvas
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 //Function to add images
 function newImage(url, left, bottom){
-    let object = document.createElement('img')
+    let object = document.createElement("img")
     object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
+    object.style.position = "fixed"
+    object.style.left = left + "px"
+    object.style.bottom = bottom + "px"
     document.body.append(object)
     return object
 }
