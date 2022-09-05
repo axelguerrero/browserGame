6c9af20ff7d1bb8248265b8cc2d55718a7f37172
axelguerrero <guerreroaxel230@yahoo.com>
2022-09-05 18:00:04 -0400

    finishsed customizing characters blueprints

--- index.js
+++ index.js
@@ -8,31 +8,71 @@ canvas.height = 600
 //fills in canvas
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+//to constantly pull our players down
+const gravity = 0.2
+
 //What we are going to be using sprites in order to get characters in the game, Sprites are essentially compilations of 2d images in order to get animations.
 // OOP, using a class to group up data to program my character
 class character {
-    constructor(position, velocity) {
+    constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
+        this.height = 150
+        this.width = 50
     }
-
+//This just makes our character appear on the screen and follows those certain criterias we added    
 draw() {
      c.fillStyle = "blue"
-     c.fillRect(this.position.x, this.position.y, 50, 150)
+     c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+
+ // updates properites to begin moving spirtes acorss the screen
+  refresh() {
+    this.draw()
+    this.position.y += this.velocity.y
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0
+    } else this.velocity.y +=  gravity
   }
 }
 
+//usable character
 let user = new character({
+   position: {
+    x: 300,
+    y: 0
+   },
+   velocity: {
     x: 0,
     y: 0
+   }
+
 })
 
 user.draw()
 
-
+//enemy player
 let ai= new character({
-    x: 600,
-    y: 100
+    position: {
+        x: 699,
+        y: 0
+       },
+       velocity: {
+        x: 0,
+        y: 0
+       }
 })
 
 ai.draw()
+
+//animation loop
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = "black"
+    c.fillRect(0,0, canvas.width, canvas.height)
+    user.refresh()
+    ai.refresh()
+} 
+
+animate()
