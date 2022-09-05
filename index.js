83e8948e53e3e70905edc6dbf7cce49d8327ad39
axelguerrero <guerreroaxel230@yahoo.com>
2022-09-04 23:10:54 -0400

    got character blocks in

--- index.js
+++ index.js
@@ -10,6 +10,29 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 //What we are going to be using sprites in order to get characters in the game, Sprites are essentially compilations of 2d images in order to get animations.
 // OOP, using a class to group up data to program my character
-// class  {
-// constructor({position, velocity})
-// }
+class character {
+    constructor(position, velocity) {
+        this.position = position
+        this.velocity = velocity
+    }
+
+draw() {
+     c.fillStyle = "blue"
+     c.fillRect(this.position.x, this.position.y, 50, 150)
+  }
+}
+
+let user = new character({
+    x: 0,
+    y: 0
+})
+
+user.draw()
+
+
+let ai= new character({
+    x: 600,
+    y: 100
+})
+
+ai.draw()
--- timer.js
+++ timer.js
@@ -1,9 +1,9 @@
 //function for our timer
 let timer = 60
 function countdown() {
-    //if our timers value is greater than 0
-        setTimeout(countdown, 1000)
-        if (timer > 0) {
+    //if our timers value is greater than 0 we will subtract one over and over
+    if (timer > 0) {    
+    setTimeout(countdown, 1000)
         timer--
         document.querySelector("#timer").innerHTML = timer
     }
