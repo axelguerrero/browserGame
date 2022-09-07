58bf7994697c4097ed00cd0be463c89fac0f78f6
axelguerrero <guerreroaxel230@yahoo.com>
2022-09-07 00:47:24 -0400

    restarted and organized my code differently got cahracters and ui

--- index.js
+++ index.js
@@ -1,93 +1,107 @@
-//Learned how to set up a canvas from https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
-const canvas = document.querySelector("canvas")
-const c = canvas.getContext("2d")
-//dimensions
-canvas.width = 1000
-canvas.height = 600
+//I spent so much time trying to get this to work that i had deleted my code and this worked
+ https://www.encodedna.com/html5/canvas/add-image-to-html5-canvas-using-javascript.htm
+// window.onload = function () {
+//     let img = new Image();
+//     img.src = "./images/background.png"
 
-//fills in canvas
-c.fillRect(0, 0, canvas.width, canvas.height);
+//     img.onload = function () {
+//         fillCanvas(img);
+//     }
 
-make_base();
+//     function fillCanvas(img) {
+//         const canvas = document.getElementById('canvas');
+//          const ctx = canvas.getContext('2d');
 
-function make_base()
-{
-  base_image = new Image();
-  base_image.src = "./images/background.png";
-  base_image.onload = function(){
-    c.drawImage(base_image, 0, 0);
-  }
-}
+//         canvas.width = img.width;
+//         canvas.height = img.height;
+//          ctx.drawImage(img, 0, 0);
+//     }
+// }
 
-//to constantly pull our players down
+// const img = new Image() {
+//     img.src = "fw"
+// }
+
+var canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.fillRect(0,0, canvas.width, canvas.height)
+
 const gravity = 0.2
 
-//What we are going to be using sprites in order to get characters in the game, Sprites are essentially compilations of 2d images in order to get animations.
-// OOP, using a class to group up data to program my character
-class character {
+class Platform {
+    constructor() {
+        this.position = {
+            x: 250,
+            y: 450
+        }
+
+        this.width = 500
+        this.height = 40
+    }
+
+    draw() {
+        ctx.fillStyle = "blue"
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const platform = new Platform()
+platform.draw()
+
+class Fighter {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
         this.height = 150
-        this.width = 50
     }
-//This just makes our character appear on the screen and follows those certain criterias we added    
-draw() {
-     c.fillStyle = "blue"
-     c.fillRect(this.position.x, this.position.y, this.width, this.height)
-  }
 
- // updates properites to begin moving spirtes acorss the screen
-  refresh() {
+     draw() {
+        ctx.fillStyle = "red"
+        ctx.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+//updates properties as it change while it moves aronud
+refresh() {
     this.draw()
     this.position.y += this.velocity.y
 
-    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-      this.velocity.y = 0
-    } else this.velocity.y +=  gravity
+    //stops from falling past 0 level
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {        
+        this.velocity.y = 0
+    } else this.velocity.y += gravity
   }
 }
-
-//usable character
-let user = new character({
-   position: {
-    x: 300,
-    y: 0
-   },
-   velocity: {
+const user = new Fighter({
+    position: {
+    x: 350,
+    y: 150
+ },
+ velocity: {
     x: 0,
     y: 0
-   }
-
+ }
 })
 
-user.draw()
+user.draw() 
 
-//enemy player
-let ai= new character({
+const ai = new Fighter({
     position: {
-        x: 699,
-        y: 0
-       },
-       velocity: {
-        x: 0,
-        y: 0
-       }
+    x: 600,
+    y: 150
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
 })
 
 ai.draw()
 
-//animation loop
-function animate() {
-    window.requestAnimationFrame(animate)
-    c.fillStyle = "black"
-    c.fillRect(0,0, canvas.width, canvas.height)
+function aniloop() {
+    window.requestAnimationFrame(aniloop) 
+    ctx.fillStyle = "black"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
     user.refresh()
     ai.refresh()
-} 
+}
 
-animate()
-
-window.addEventListener("keydown", (event) => {
-    console.log(event.key)
-})
+aniloop()
